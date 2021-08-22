ab0713f7e114a90b2c8b1dc054fcee60053a62c6
NYYuHao <NYYuHao@gmail.com>
2021-08-22 01:06:25 -0400

    Making use of imports with components

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './component/App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <h1>Gacha Website</h1>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
