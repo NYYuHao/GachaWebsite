c7f3f7a608bf06ef4cfcc0bedfcbf206be609418
NYYuHao <NYYuHao@gmail.com>
2021-08-23 17:30:19 -0400

    index shows App, App handles routing. Added roll page

--- src/component/App.js
+++ src/component/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import RollPage from './RollPage';
 import Card from './Card';
 
 export default class App extends React.Component {
     render() {
         return (
-            <div className="app">
-                <h1>Gacha Website</h1>
-                <Card />
-            </div>
+            <Router>
+                <div className="app">
+                    <Link to="/">Roll</Link>
+                    <Link to="/card">Card</Link>
+                    <Switch>
+                        <Route exact path='/'>
+                            <RollPage />
+                        </Route>
+                        <Route path='/card'>
+                            <Card />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
         );
     }
 };
---
+++ src/component/RollPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from './Card';
+
+export default class RollPage extends React.Component {
+    render() {
+        return (
+            <div className="roll-page">
+                <h1>Gacha Website</h1>
+                <Card />
+            </div>
+        );
+    }
+}
