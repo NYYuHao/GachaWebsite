908e444f9aa1fefe99d732b6195ecb9e2c7ecff6
NYYuHao <NYYuHao@gmail.com>
2021-08-26 01:07:05 -0400

    Trying to render multiple card objects

--- src/component/CollectionPage.js
+++ src/component/CollectionPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Card from './Card';
 
+let cards = [100, 200, 300];
+let cardsList = cards.map((card) => <Card id={card} />);
+
 export default class CollectionPage extends React.Component {
     render() {
         return (
             <div className="collection-page">
                 <h1>Collection</h1>
-                <Card />
+                {cardsList}
             </div>
         );
     }
