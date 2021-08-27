c8a3d8393ebc0d9eba052af7593069ad80d5909e
NYYuHao <NYYuHao@gmail.com>
2021-08-27 01:11:04 -0400

    Started anilist character fetching logic

---
+++ src/logic/anilist.js
@@ -0,0 +1,33 @@
+const fetch = require('node-fetch');
+
+function getCharacter(id) {
+    let query = `
+        query ($id: Int) {
+            Character (id: $id) {
+                name {
+                    full
+                }
+                favourites
+                image {
+                    large
+                }
+            }
+        }`;
+
+    var url = 'https://graphql.anilist.co',
+    options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            query: query,
+            variables: {id: id}
+        })
+    };
+  
+    return fetch(url, options).then((res) => {return res.json()});
+}
+
+getCharacter(500).then(console.log);
