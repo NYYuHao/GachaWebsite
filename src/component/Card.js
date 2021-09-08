2a8dc2f094c200c5b4d28386892a61b514dd8141
NYYuHao <NYYuHao@gmail.com>
2021-09-08 00:38:17 -0400

    Button logic so buttons don't render in collection page

--- src/component/Card.js
+++ src/component/Card.js
@@ -3,14 +3,19 @@ import './Card.css';
 
 export default class Card extends React.Component {
     render() {
+        let claimButton = this.props.handleClaim ? 
+            <button onClick={this.props.handleClaim}>Claim</button> : null;
+        let skipButton = this.props.handleSkip ? 
+            <button onClick={this.props.handleSkip}>Skip</button> : null;
+
         return (
             <div className="card">
                 <h2>{this.props.name}</h2>
                 <p>{this.props.media}</p>
                 <p>{this.props.value}</p>
                 <img src={this.props.image} alt="Character"/>
-                <button onClick={this.props.handleClaim}>Claim</button>
-                <button onClick={this.props.handleSkip}>Skip</button>
+                {claimButton}
+                {skipButton}
             </div>
         );
     };
