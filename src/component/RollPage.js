32155f34c658887df4f92c402d0cbae8fd314fe8
NYYuHao <NYYuHao@gmail.com>
2021-09-16 03:59:44 -0400

    Messing with transitions

--- src/component/RollPage.js
+++ src/component/RollPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Spring, animated} from 'react-spring';
+import {Transition, config, animated} from 'react-spring';
 import Card from './Card';
 import './RollPage.css';
 
@@ -15,25 +15,31 @@ export default class RollPage extends React.Component {
 
     render() {
         // Build card components based on props character data
-        let currentCard = this.renderCard(this.props.currentCharacter)
-        let nextCard = this.renderCard(this.props.nextCharacter)
+        let currentCard = this.renderCard(this.props.currentCharacter);
+        let nextCard = this.renderCard(this.props.nextCharacter);
 
         return (
             <div className="roll-page">
                 <h1>Gacha Website</h1>
 
-                <Spring
-                    from={{opacity: 0}}
-                    to={{opacity: 1}}>
-                    {styles => (
-                        <animated.div style={styles} className="rolls">
-                            {currentCard}
-                            <button onClick={this.props.handleClaim}>Claim</button>
-                            <button onClick={this.props.handleSkip}>Skip</button>
-                            {nextCard}
-                        </animated.div>
-                    )}
-                </Spring>
+                <div className="rolls">
+                    <Transition            
+                        items={true}
+                        from={{ opacity: 0 }}
+                        enter={{ opacity: 1 }}
+                        leave={{ opacity: 0 }}
+                        delay={200}
+                        config={config.molasses}>
+                        {(styles, items) => (
+                            <animated.div styles={styles}>
+                                {currentCard}
+                            </animated.div>
+                        )}
+                    </Transition>
+                    <button onClick={this.props.handleClaim}>Claim</button>
+                    <button onClick={this.props.handleSkip}>Skip</button>
+                    {nextCard}
+                </div>
             </div>
         );
     }
