fbc3ff7a3c5b09a9fc636520305c376786d3d909
NYYuHao <NYYuHao@gmail.com>
2021-09-17 17:50:43 -0400

    Removed usage of react-spring, can hopefully manually update

--- src/component/RollPage.js
+++ src/component/RollPage.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import {Transition, config, animated} from 'react-spring';
 import Card from './Card';
 import './RollPage.css';
 
 export default class RollPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentCharacter: {},
+            nextCharacter: {}
+        }
+    }
+
     // Given a character, return a card component
     renderCard(character) {
         return <Card 
@@ -13,29 +20,28 @@ export default class RollPage extends React.Component {
             image={character.image}/>
     }
 
+    componentDidUpdate(prevProps) {
+        if (prevProps.currentCharacter !== this.props.currentCharacter) {
+            // TODO: Handle animation
+            console.log('Switching characters');
+            this.setState({
+                currentCharacter: this.props.currentCharacter,
+                nextCharacter: this.props.nextCharacter
+            });
+        }
+    }
+
     render() {
         // Build card components based on props character data
-        let currentCard = this.renderCard(this.props.currentCharacter);
-        let nextCard = this.renderCard(this.props.nextCharacter);
+        let currentCard = this.renderCard(this.state.currentCharacter);
+        let nextCard = this.renderCard(this.state.nextCharacter);
 
         return (
             <div className="roll-page">
                 <h1>Gacha Website</h1>
 
                 <div className="rolls">
-                    <Transition            
-                        items={true}
-                        from={{ opacity: 0 }}
-                        enter={{ opacity: 1 }}
-                        leave={{ opacity: 0 }}
-                        delay={200}
-                        config={config.molasses}>
-                        {(styles, items) => (
-                            <animated.div styles={styles}>
-                                {currentCard}
-                            </animated.div>
-                        )}
-                    </Transition>
+                    {currentCard}
                     <button onClick={this.props.handleClaim}>Claim</button>
                     <button onClick={this.props.handleSkip}>Skip</button>
                     {nextCard}
