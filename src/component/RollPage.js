b03d64249fb08def4ca43115579f3b095004bfb5
NYYuHao <NYYuHao@gmail.com>
2021-09-20 01:44:40 -0400

    Animations now trigger based on transitioning state, removed previous card

--- src/component/RollPage.js
+++ src/component/RollPage.js
@@ -6,7 +6,7 @@ export default class RollPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            previousCharacter: {},
+            transitioning: false,
             currentCharacter: props.currentCharacter,
             nextCharacter: props.nextCharacter
         }
@@ -21,35 +21,51 @@ export default class RollPage extends React.Component {
             image={character.image}/>
     }
 
+    // When new props are given, change the state and handle animations
     componentDidUpdate(prevProps) {
         if (prevProps.currentCharacter !== this.props.currentCharacter) {
-            console.log('Switching characters');
-            this.setState({
-                previousCharacter: prevProps.currentCharacter,
-                currentCharacter: this.props.currentCharacter,
-                nextCharacter: this.props.nextCharacter
-            });
+            // Avoid animation on first update with character info
+            if (Object.keys(prevProps.currentCharacter).length === 0) {
+                this.setState({
+                    transitioning: false,
+                    currentCharacter: this.props.currentCharacter,
+                    nextCharacter: this.props.nextCharacter
+                });
+            }
+            // Otherwise trigger transition
+            else {
+                this.setState({transitioning: true});
+            }
         }
+    }
+
+    // When animations finish, change transition state and update characters
+    onTransitionFinish() {
+        this.setState({
+            transitioning: false,
+            currentCharacter: this.props.currentCharacter,
+            nextCharacter: this.props.nextCharacter
+        });
     }
 
     render() {
         // Build card components based on props character data
-        let previousCard = Object.keys(this.state.previousCharacter).length !== 0 ?
-            this.renderCard(this.state.previousCharacter) : null;
         let currentCard = this.state.currentCharacter ? 
             this.renderCard(this.state.currentCharacter) : null;
         let nextCard = this.state.nextCharacter ?
             this.renderCard(this.state.nextCharacter) : null;
 
+        // Define classNames based on whether the cards are in transition state
+        let currentCardClasses = "current-card";
+        currentCardClasses += this.state.transitioning ? " is-leaving" : "";
+
+        // TODO: When cards are transitioning, disable these buttons
         return (
             <div className="roll-page">
                 <h1>Gacha Website</h1>
 
                 <div className="rolls">
-                    <div key={this.state.previousCharacter.id} className="previous-card">
-                        {previousCard}
-                    </div>
-                    <div className="current-card">
+                    <div onAnimationEnd={() => this.onTransitionFinish()} className={currentCardClasses}>
                         {currentCard}
                     </div>
                     <button onClick={this.props.handleClaim}>Claim</button>
